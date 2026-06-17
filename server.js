69d0a1ef64714aeb8849bec176d8473988130ae1
Mork56 <68319010006@thonburi.ac.th>
2026-06-17 14:41:32 +0700

    อัพเดท

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-"" 
+require('dotenv').config();
+
+const express = require('express');
+const supabase = require('./supabase');
+
+const app = express();
+
+app.get('/products', async (req, res) => {
+  const { data, error } = await supabase
+    .from('products')
+    .select('*');
+
+  if (error) {
+    return res.status(500).json(error);
+  }
+
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
